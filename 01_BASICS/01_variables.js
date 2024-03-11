53810a0b58968ff8ec8d0dee2dc55855598f1706
takemeforward <108937541+takemeforward@users.noreply.github.com>
2024-03-11 18:50:06 +0000

    Understand different types of variable

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,10 @@
+const neighbourId = 1234
+let neighbourEmail = "vishal@vishal.com"
+var neighbourName = "vishal" // Use of var is not that appreaciated because of its always global scope
+neigbourAge = 34
+
+// neighbourId = 222; // changing a constant is not allowed
+
+// console.log(neighbourId); 
+
+console.table([neighbourId, neighbourEmail, neighbourName, neigbourAge])
