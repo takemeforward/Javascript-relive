781963955ba67fa31cd205a9913fdb281f927ad2
takemeforward <108937541+takemeforward@users.noreply.github.com>
2024-03-14 00:30:41 +0000

    Learn about different types of params of string.

---
+++ 01_BASICS/05_string.js
@@ -0,0 +1,22 @@
+let myName = "Vishal"
+
+let age = 23
+
+// console.log("Hello my name is " + myName + " and I'm " + age + " years Old"); // Avoid writing code this way
+
+// console.log(`Hello my name is ${myName} and I'm ${age} years Old`);
+
+
+console.log(myName.length)
+console.log(myName.toUpperCase())
+console.log(myName)
+console.log(myName.charAt(3))
+console.log(myName.indexOf('n'))
+console.log(myName.concat(" Jha"))
+console.log(myName.search('l'))
+
+let myString = new String("what is this ");
+// String made using constructor can be used in either ways
+console.log(myString.valueOf()) 
+console.log(myString.toString())
+
